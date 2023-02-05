6d2f468e054f500fcc3ae82049d6a89baf85d001
Houasnia Aymen Ahmed <houasnia.aymen.a@hns-re2sd.dz>
2023-02-05 18:35:25 +0100

    Update Function.py

--- Function.py
+++ Function.py
@@ -1,3 +1,4 @@
+# This file contains some usefull function for the game
 import numpy as np
 
 def displayWelcome() -> None:
@@ -17,6 +18,7 @@ def displayWelcome() -> None:
     print('|                                                                      |')
     print('','='*70)
 
+# Checks the validity of value ( must be between 1-9)
 def checkValue(value):
   check = True
   if value < 1 or value > 9:
@@ -32,32 +34,42 @@ def checkValidPosition(position):
 
 # Function that checks if a row is correctly complete
 def checkRow(board,row_position,value,disp=False):
+    
   check = True
   countValueInRow = board[row_position-1].tolist().count(value)
+    
   if countValueInRow > 1:
     check = False
   return (check,countValueInRow)
 
+# Function that checks if a col is correctly complete
 def checkColumn(board,column_position,value,disp=False):
+    
   check = True
   column = [row[column_position-1] for row in board]
   countValueInCol = column.count(value)
+
   if countValueInCol > 1:
     check = False
   return (check,countValueInCol)
 
+# Checks if the value is duplicated in a row or a col
 def checkPosition(board,position,value):
   check = True
+
   if (checkColumn(board,position[1],value)[0] == False):
     print("Be carfull, This item is already in this column")
     check = False
+    
   elif (checkRow(board,position[0],value)[0] == False):
     print("Be carfull, This item is already in this row")
     check = False
+    
   else:
     check = True
   return check
 
+# Check if the value is duplicated in the 3x3 square
 def checkSquare(miniboard,value,disp=False):
   check = True
   count = np.count_nonzero(miniboard == value)
@@ -68,12 +80,14 @@ def checkSquare(miniboard,value,disp=False):
       print("Be carfull, This item is already in this Square")
   return check
 
+# Find the square of a given position
 def findSquare(position):
   row = position[0]
   col = position[1]
   square = 0
 
   if row in [0,1,2]:
+        
     if col in [0,1,2]:
       square = 1
     elif col in [3,4,5]:
@@ -84,6 +98,7 @@ def findSquare(position):
       print("Error row (1-3)")
 
   elif row in [3,4,5]:
+    
     if col in [0,1,2]:
       square = 4
     elif col in [3,4,5]:
@@ -94,6 +109,7 @@ def findSquare(position):
       print("Error row (4-6)")
 
   elif row in [6,7,8]:
+    
     if col in [0,1,2]:
       square = 7
     elif col in [3,4,5]:
@@ -107,7 +123,9 @@ def findSquare(position):
 
   return square
 
+# Get the 3x3 square by it's position
 def getSquare(board,square):
+    
   miniboard = None
   if square == 1:
     miniboard = board[0:3,0:3]
@@ -131,26 +149,33 @@ def getSquare(board,square):
     print('ERROR!!')
   return miniboard
 
+# Gives the number of cells that are still non-filled
 def checkBoard(board):
   check = True
   fcheck, counter = checkFull(board)
+    
   if  fcheck == False:
     check = False
-    print('There is still ',counter,'unfilled positions on the board')
+    print('There is still %d unfilled positions on the board'%counter)
     return check
+
   for i in range(1,10):
     for j in range(1,10):
+        
       ccheck, _ = checkColumn(board,i,j)
       rcheck, _ = checkRow(board,i,j)
       scheck = checkSquare(getSquare(board,i),j)
+    
       if ccheck == False or rcheck == False or scheck == False:
         check = False
         break
+        
     if check == False:
       break
   
   return check
 
+# Checks wether the function is full or not ( other way to check is in the boardGenerator.py file )
 def checkFull(board):
   check = True
   counter = 0
