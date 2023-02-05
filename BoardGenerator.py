aeb27bdda33af40fa7e4e3dfa9b1ceeefaa63223
Houasnia Aymen Ahmed <houasnia.aymen.a@hns-re2sd.dz>
2023-02-05 18:14:31 +0100

    Commenting the code
    Add some comments to the code to make it easier to understand.

--- BoardGenerator.py
+++ BoardGenerator.py
@@ -1,30 +1,50 @@
+# import important libraries
 import random
 from random import shuffle
 
 class Board:
 
   def __init__(self) -> None:
+     # Generates the board when an instance of the class Board is created
      self.grid = self.generate()
+    
+    # Function that checks for if the grid is full or not, by going through all the board cells
   def checkGrid(self,grid):
       for row in range(0,9):
           for col in range(0,9):
+            
             if grid[row][col]==0:
               return False
+
       return True
-      
+    
+    
+  # Function that iterates through all the cells recursively to make a playable sudoku board.
   def fillGrid(self,grid):
       for i in range(0,81):
         row=i//9
         col=i%9
+        
         if grid[row][col]==0:
+          # The first 3 variables for the rows, cols and 3x3 square
           row_set = set(grid[row])
           col_set = set([grid[0][col],grid[1][col],grid[2][col],grid[3][col],grid[4][col],grid[5][col],grid[6][col],grid[7][col],grid[8][col]])
           subgrid_set = set(number for sublist in grid[row//3*3:row//3*3+3] for number in sublist[col//3*3:col//3*3+3])
+          
+          # To make the generator eve, more efficient and optimized, this variable is used to store 
+          # only the values that haven't been used in the board ( following the rules of sudoku )
           valid_values = set(range(1, 10)) - (row_set | col_set | subgrid_set)
           valid_values = list(valid_values)
+          
+          # Shuffle the values to make sure we get a diffrent values, either way we would always
+          # Start filling the board from 1 to 9, and we would get always the same board
           shuffle(valid_values)
+          
+          # Fill the board making sure that the value isn't duplicated
           for value in valid_values:
               grid[row][col]=value
+              
+              # Make sure the generator stops when the board is full
               if self.checkGrid(grid):
                   return True
               else:
@@ -32,6 +52,7 @@ class Board:
                     return True
           break
       grid[row][col]=0
+      
   def generate(self):
     grid =[[0, 0, 0, 0, 0, 0, 0, 0, 0],
            [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -46,6 +67,8 @@ class Board:
     self.fillGrid(grid)
     return grid
 
+  # Remove some values from the board to make it playable ( either way we would have a solved board ).
+  # The number of values is removed based on the level the user wants to play. if he don't choose or something went wrong, level 2 is chosen automatically
   def Level(self,board=None,mode = '2'):
 
     if board is None:
@@ -69,6 +92,7 @@ class Board:
       tempBoard[row][col] = 0
     return tempBoard
 
+  # Display the board in a way that is so close to the actual sudoky board
   def displayBoard(self,board=None):
     if board is None:
       board = self.grid
